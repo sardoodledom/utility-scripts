02e43651c212460036a2cc8a78e53581a2c389b5
Dave Spano <daspano@cisco.com>
2017-08-22 23:44:45 -0400

    Rename backup script
    Renamed it to snake case, so it could be imported by unittests

--- python/mysql_backup/mysql_backup.py
+++ python/mysql_backup/mysql_backup.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-
 import argparse
 import paramiko
 import logging
@@ -9,39 +7,22 @@ from datetime import datetime
 
 LOG = logging.getLogger(__name__)
 SSH_USER = "centos"
-BACKUP_DIR = "/tmp/"
 
 
-def create_connection(hostname, username):
-    """
-    Create a connection to the remote host
-
-    :param hostname:
-    :param username:
-    :return:
-    """
-    LOG.info('Trying to connect')
-    ssh = paramiko.SSHClient()
-    ssh.load_system_host_keys()
-    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    ssh.connect(hostname, username, timeout=180, look_for_keys=True)
-
-    return ssh
-
-
-def backup_database(ssh, database, BACKUP_DIR):
+def backup_database(ssh, database, backup_dir):
     """
     Use paramiko to run mysql dump on the remote host
 
     :param ssh:
     :param database:
-    :param BACKUP_DIR:
+    :param backup_dir:
     :return back_path:
     """
 
     backup_time = datetime.datetime.now().strftime('%m-%d-%Y-%H:%M:%S')
-    backup_file_path = '{0}/{1}.sql'.format(BACKUP_DIR, backup_time)
-    mysqldump_cmd = 'sudo mysqldump {0} > {1}'.format(database, backup_file_path)
+    backup_file_path = '{0}/{1}.sql'.format(backup_dir, backup_time)
+    mysqldump_cmd = 'sudo mysqldump {0} > {1}'.format(database,
+                                                      backup_file_path)
     try:
         _, stdout, stderr = ssh.exec_command(mysqldump_cmd)
         if stderr is not '':
@@ -68,7 +49,7 @@ def compress_db_backup(ssh, backup_file_path):
 
     try:
         _, stdout, _ = ssh.exec_command(compress_cmd)
-        LOG.info('stdout: {0}'.format(stdout))
+        LOG.info('stdout: {0}'.format(stdout.read().decode()))
         file_list = ssh.listdir()
     except paramiko.ssh_exception.SSHException as e:
         LOG.error('Connection to host failed with error'
@@ -77,30 +58,22 @@ def compress_db_backup(ssh, backup_file_path):
     return compressed_path if compressed_path in file_list else None
 
 
-def create_remote_path(ssh, path):
+def create_connection(hostname, username):
     """
-    Create remote backup path if it doesn't exist
+    Create a connection to the remote host
 
-    :param ssh:
-    :param path:
+    :param hostname:
+    :param username:
     :return:
     """
-    check_remote_path = 'sudo test -d {0}'.format(path)
-    create_path_cmd = 'mkdir -p {0}'.format(path)
-    try:
-        _, stdout, _ = ssh.exec_command(check_remote_path)
+    LOG.info('Trying to connect')
+    ssh = paramiko.SSHClient()
+    ssh.load_system_host_keys()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect(hostname=hostname, username=username, timeout=180,
+                look_for_keys=True)
 
-        # We should get an exit status of 1 if the path doesn't exist
-        if stdout.channel.recv_exit_status == 1:
-            _, stdout, stderr = ssh.exec_command(create_path_cmd)
-            if stderr is not '':
-                LOG.error('stderr is {0}'.format(stderr))
-                return False
-    except paramiko.ssh_exception.SSHException as e:
-        LOG.error('Connection to host failed with error'
-                  '{0}'.format(e))
-
-    return True
+    return ssh
 
 
 def create_local_path(path):
@@ -113,6 +86,28 @@ def create_local_path(path):
 
     if not os.path.exists(path):
         os.makedirs(path)
+
+
+def create_remote_path(ssh, path):
+    """
+    Create remote backup path if it doesn't exist
+
+    :param ssh:
+    :param path:
+    :return:
+    """
+    create_path_cmd = 'sudo mkdir -p {0}'.format(path)
+    try:
+        _, stdout, stderr = ssh.exec_command(create_path_cmd)
+
+        # We should get an exit status of 1 if the path doesn't exist
+        if stderr.channel.recv_exit_status == 1:
+            # LOG.error('stderr is {0}'.format(stderr.read().decode()))
+            return False
+        return path
+    except paramiko.ssh_exception.SSHException as e:
+        LOG.error('Connection to host failed with error'
+                  '{0}'.format(e))
 
 
 def get_backup_file(ssh, local_path, remote_path):
@@ -134,10 +129,22 @@ def main():
     parser = argparse.ArgumentParser(
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument(
+        '--local-dir', action='store',
+        default='/tmp',
+        help="The backup directory on the local host")
+    parser.add_argument(
+        '--remote-dir', action='store',
+        default='/tmp',
+        help="The backup directory on the remote database host")
+    parser.add_argument(
         '-v', '--verbose', action='count', default=0,
         help="Increase verbosity (specify multiple times for more)")
+    args = parser.parse_args()
 
     log_level = logging.INFO
+    if args.verbose >= 1:
+        log_level = logging.DEBUG
+
     format = '%(asctime)s - %(levelname)s - %(message)s'
     logging.basicConfig(format=format, datefmt='%m-%d %H:%M', level=log_level)
 
